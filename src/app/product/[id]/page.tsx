c23398e4701bb494b5c3285d23f94598f8eb4cd5
saurabhkr66 <sauravkum420@gmail.com>
2025-07-23 10:39:19 +0530

    fixes

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,14 +1,17 @@
-// app/product/[id]/page.tsx
 import axios from "axios";
 import { ProductDetailsPage } from "@/components/ProductDetailsPage";
 import { Product } from "@/types/product";
 
-export default async function ProductPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const res = await axios.get(`https://dummyjson.com/products/${params.id}`);
+interface ProductPageProps {
+  params: {
+    id: string; // route param is always a string
+  };
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
+  const productId = Number(params.id); // Convert to number
+
+  const res = await axios.get(`https://dummyjson.com/products/${productId}`);
   const product: Product = res.data;
 
   return <ProductDetailsPage product={product} />;
