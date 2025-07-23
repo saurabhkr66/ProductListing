c54dcd76ba80d2804098e872210aea9e2c4b4069
saurabhkr66 <sauravkum420@gmail.com>
2025-07-23 11:20:23 +0530

    fix

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -19,17 +19,38 @@
 
 // src/app/product/[id]/page.tsx
 
+// import axios from "axios";
+// import { ProductDetailsPage } from "@/components/ProductDetailsPage";
+// import { Product } from "@/types/product";
+
+// type ProductPageProps = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+// export default async function ProductPage({ params }: ProductPageProps) {
+//   const res = await axios.get(`https://dummyjson.com/products/${params.id}`);
+//   const product: Product = res.data;
+
+//   return <ProductDetailsPage product={product} />;
+// }
+// src/app/product/[id]/page.tsx
+
 import axios from "axios";
 import { ProductDetailsPage } from "@/components/ProductDetailsPage";
 import { Product } from "@/types/product";
 
-type ProductPageProps = {
+interface PageProps {
   params: {
     id: string;
   };
-};
+  searchParams?: {
+    [key: string]: string | string[] | undefined;
+  };
+}
 
-export default async function ProductPage({ params }: ProductPageProps) {
+export default async function ProductPage({ params }: PageProps) {
   const res = await axios.get(`https://dummyjson.com/products/${params.id}`);
   const product: Product = res.data;
 
