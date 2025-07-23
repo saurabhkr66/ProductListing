50aa6a13fd06630e56d054f540aa9d70871b0f66
saurabhkr66 <sauravkum420@gmail.com>
2025-07-23 11:01:15 +0530

    fix

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,10 +2,9 @@ import axios from "axios";
 import { ProductDetailsPage } from "@/components/ProductDetailsPage";
 import { Product } from "@/types/product";
 
-export default async function ProductPage(props: {
-  params: { id: string };
-}) {
-  const { params } = await props; // ✅ Just await it, no need to type as Promise
+// ✅ Page Component
+export default async function ProductPage(props: Promise<{ params: { id: string } }>) {
+  const { params } = await props; // ✅ Await props
   const productId = Number(params.id);
 
   const res = await axios.get(`https://dummyjson.com/products/${productId}`);
